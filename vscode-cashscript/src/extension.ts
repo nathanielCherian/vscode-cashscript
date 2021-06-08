a3afd53c15fddfe9ba1ea4f4e6d4a4c234797316
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-06-08 14:10:03 -0700

    compile command

--- vscode-cashscript/src/extension.ts
+++ vscode-cashscript/src/extension.ts
@@ -1,10 +1,17 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 import { ExtensionContext, workspace } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 import CashscriptHoverProvider from './CashscriptHoverProvider';
 import CashscriptSignatureCompleter from './CashscriptSignatureCompleter';
 import CashscriptCompletionProvider from './CashscriptCompletionProvider';
+import { compileFile } from 'cashc';
+import {
+	asmToScript,
+	calculateBytesize,
+	countOpcodes,
+  } from '@cashscript/utils';
 
 let client: LanguageClient;
 
@@ -46,8 +53,37 @@ export function activate(context: ExtensionContext){
         clientOptions
     )
 
-    // Subscribe to events here
+    
+    // Subscribe to Commands here
+    context.subscriptions.push(vscode.commands.registerCommand('cashscript.compile', () => {
+        var filename = vscode.window.activeTextEditor.document.fileName;
+        try{
+            const artifact = compileFile(filename);
+            const artifactJSON = JSON.stringify(artifact, null, 2);
+            filename = filename.split('.').slice(0, -1).join('.') + ".json" 
+            fs.writeFile(filename, artifactJSON, (err) => {
+                if(err){
+                    throw err
+                }
+            });
+            const script = asmToScript(artifact.bytecode);
+            const opcount = countOpcodes(script);
+            const bytesize = calculateBytesize(script);
+            if(opcount > 201){
+                vscode.window.showWarningMessage('Warning: Your contract\'s opcount is over the limit of 201 and will not be accepted by the BCH network');
+            }
+            if(bytesize > 520){
+                vscode.window.showWarningMessage('Warning: Your contract\'s bytesize is over the limit of 520 and will not be accepted by the BCH network');
+            }
+            vscode.window.showInformationMessage(`Compiled contract to artifact '${path.basename(filename)}'`);
+            console.log(`Compiled contact to ${filename}`);
+        }catch(e){
+            vscode.window.showErrorMessage(`Unable to compile '${path.basename(filename)}', ` + e);
+        }
+    }));
 
+
+    // Subscribe to events here
     vscode.languages.registerHoverProvider('cashscript', new CashscriptHoverProvider())
 	vscode.languages.registerSignatureHelpProvider('cashscript', new CashscriptSignatureCompleter(), '(');
     vscode.languages.registerCompletionItemProvider('cashscript', new CashscriptCompletionProvider(), '.');
--- vscode-cashscript/src/server.ts
+++ vscode-cashscript/src/server.ts
@@ -29,8 +29,6 @@ connection.onInitialize((params: InitializeParams) => {
 });
 
 
-
-
 /**
  * On document change
  *  - validate
